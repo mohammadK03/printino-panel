c6906eebb68160036b0df4cf7c77b9d496aee5d0
unknown <mohammadhoseinkazemi1103@gmail.com>
2022-12-09 19:27:21 +0330

    error fixed

--- nuxt-adminpanel/App.js
+++ nuxt-adminpanel/App.js
@@ -4,7 +4,6 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\assets\\fonts.css'
 
@@ -13,8 +12,6 @@ import '..\\assets\\global.css'
 import '..\\assets\\icon.css'
 
 import '..\\node_modules\\izitoast\\dist\\css\\iziToast.css'
-
-import '..\\node_modules\\vuetify\\dist\\vuetify.css'
 
 import _2d217e9e from '..\\layouts\\auth.vue'
 import _6f6c098b from '..\\layouts\\default.vue'
@@ -55,7 +52,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -106,6 +103,10 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
+    },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
     },
   },
 
@@ -191,10 +192,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- nuxt-adminpanel/components/index.js
+++ nuxt-adminpanel/components/index.js
@@ -1,16 +1,16 @@
-export { default as ImageGallery } from '../..\\components\\gallery\\imageGallery.vue'
-export { default as ImageGalleryDialog } from '../..\\components\\gallery\\imageGalleryDialog.vue'
-export { default as ProductAttribute } from '../..\\components\\product\\attribute.vue'
-export { default as ProductInfo } from '../..\\components\\product\\productInfo.vue'
-export { default as ProductComments } from '../..\\components\\attributes_overview\\productComments.vue'
-export { default as ProductOverview } from '../..\\components\\attributes_overview\\productOverview.vue'
-export { default as ProductSpecifications } from '../..\\components\\attributes_overview\\productSpecifications.vue'
-export { default as AddressAdd } from '../..\\components\\address\\addressAdd.vue'
-export { default as AddressAll } from '../..\\components\\address\\addressAll.vue'
-export { default as AddressMap } from '../..\\components\\address\\addressMap.vue'
-export { default as TransactionOrder } from '../..\\components\\transaction\\order.vue'
-export { default as TransactionTranactionInfo } from '../..\\components\\transaction\\tranactionInfo.vue'
-export { default as TransactionTransactionbacup } from '../..\\components\\transaction\\transactionbacup.vue'
+export const ImageGallery = () => import('../..\\components\\gallery\\imageGallery.vue' /* webpackChunkName: "components/image-gallery" */).then(c => wrapFunctional(c.default || c))
+export const ImageGalleryDialog = () => import('../..\\components\\gallery\\imageGalleryDialog.vue' /* webpackChunkName: "components/image-gallery-dialog" */).then(c => wrapFunctional(c.default || c))
+export const ProductAttribute = () => import('../..\\components\\product\\attribute.vue' /* webpackChunkName: "components/product-attribute" */).then(c => wrapFunctional(c.default || c))
+export const ProductInfo = () => import('../..\\components\\product\\productInfo.vue' /* webpackChunkName: "components/product-info" */).then(c => wrapFunctional(c.default || c))
+export const ProductComments = () => import('../..\\components\\attributes_overview\\productComments.vue' /* webpackChunkName: "components/product-comments" */).then(c => wrapFunctional(c.default || c))
+export const ProductOverview = () => import('../..\\components\\attributes_overview\\productOverview.vue' /* webpackChunkName: "components/product-overview" */).then(c => wrapFunctional(c.default || c))
+export const ProductSpecifications = () => import('../..\\components\\attributes_overview\\productSpecifications.vue' /* webpackChunkName: "components/product-specifications" */).then(c => wrapFunctional(c.default || c))
+export const AddressAdd = () => import('../..\\components\\address\\addressAdd.vue' /* webpackChunkName: "components/address-add" */).then(c => wrapFunctional(c.default || c))
+export const AddressAll = () => import('../..\\components\\address\\addressAll.vue' /* webpackChunkName: "components/address-all" */).then(c => wrapFunctional(c.default || c))
+export const AddressMap = () => import('../..\\components\\address\\addressMap.vue' /* webpackChunkName: "components/address-map" */).then(c => wrapFunctional(c.default || c))
+export const TransactionOrder = () => import('../..\\components\\transaction\\order.vue' /* webpackChunkName: "components/transaction-order" */).then(c => wrapFunctional(c.default || c))
+export const TransactionTranactionInfo = () => import('../..\\components\\transaction\\tranactionInfo.vue' /* webpackChunkName: "components/transaction-tranaction-info" */).then(c => wrapFunctional(c.default || c))
+export const TransactionTransactionbacup = () => import('../..\\components\\transaction\\transactionbacup.vue' /* webpackChunkName: "components/transaction-transactionbacup" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
